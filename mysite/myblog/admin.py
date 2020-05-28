f08222e2758623d4d3a74f3730a99c1beff9fb1a
Donatas Noreika <donoras@gmail.com>
2020-05-28 11:37:38 +0300

    Pridėtas komentarų modelis, jis įdėtas į admin puslapį. Dar pridėtas komentarų skaitiklis

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Straipsnis
+from .models import Straipsnis, Komentaras
 
 class StraipsnisAdmin(admin.ModelAdmin):
     list_display = ('pavadinimas', 'autorius', 'laikas', 'tekstas')
 
+class KomentarasAdmin(admin.ModelAdmin):
+    list_display = ('straipsnis_id', 'vardas', 'el_pastas', 'laikas', 'komentaras')
+
 admin.site.register(Straipsnis, StraipsnisAdmin)
+admin.site.register(Komentaras, KomentarasAdmin)
--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from datetime import datetime
+
 
 # Create your models here.
 
 class Straipsnis(models.Model):
     pavadinimas = models.CharField('Pavadinimas', max_length=200)
     autorius = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    laikas = models.DateTimeField('Sukurta: ', null=True, blank=True)
+    laikas = models.DateTimeField('Sukurta: ', null=True, blank=True, default=datetime.now)
     tekstas = HTMLField('Tekstas', null=True)
+
+    @property
+    def komentaru_skaicius(self):
+        return len(Komentaras.objects.filter(straipsnis_id=self.pk))
+
 
     def __str__(self):
         return f"{self.pavadinimas} {self.autorius} {self.laikas}"
+
+
+class Komentaras(models.Model):
+    straipsnis_id = models.ForeignKey(Straipsnis, on_delete=models.SET_NULL, null=True)
+    vardas = models.CharField('Vardas', max_length=200)
+    el_pastas = models.EmailField('El. paštas (nebūtina)', null=True, blank=True)
+    laikas = models.DateTimeField('Sukurta: ', null=True, blank=True, default=datetime.now)
+    komentaras = models.TextField("Komentaras")
