07d5972b6de5bc922150bb507ef3456396da8dad
Donatas Noreika <donoras@gmail.com>
2020-05-28 11:42:55 +0300

    Admin puslapyje komentarai pridėti prie straipsnių

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -5,8 +5,12 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Straipsnis, Komentaras
 
+class KomentarasInline(admin.TabularInline):
+    model = Komentaras
+
 class StraipsnisAdmin(admin.ModelAdmin):
     list_display = ('pavadinimas', 'autorius', 'laikas', 'tekstas')
+    inlines = [KomentarasInline]
 
 class KomentarasAdmin(admin.ModelAdmin):
     list_display = ('straipsnis_id', 'vardas', 'el_pastas', 'laikas', 'komentaras')
