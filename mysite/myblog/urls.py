d32d2614e6792ee73cb2a348634355da576f5744
Donatas Noreika <donoras@gmail.com>
2020-05-28 10:52:24 +0300

    Pridėtas tinklaraščio straipsnių puslapis.

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('straipsniai/', views.StraipsnisListView.as_view(), name='straipsniai'),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Straipsnis
 
 # Create your views here.
 
@@ -7,4 +9,6 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-
+class StraipsnisListView(generic.ListView):
+    model = Straipsnis
+    template_name = 'straipsniai.html'
